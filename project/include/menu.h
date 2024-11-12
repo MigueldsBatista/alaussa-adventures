9064656551aed93e893178f86854cbcb6a0e42e5
Rafa <ralb@cesar.school>
2024-11-11 23:03:49 -0300

    ajeitei struct txt

--- project/include/menu.h
+++ project/include/menu.h
@@ -24,7 +24,7 @@ typedef struct {
     SDL_Point posicao;
     SDL_Color cor;
     char texto[256];
-} Texto;
+} Text;
 
 // Estrutura para armazenar as informações dos jogadores
 // Definição da estrutura Jogador
